036c5f4c6a92b6dba010a8c7c78b08c9b3ba536a
s0041097 <nkejriwal@rgare.com>
2022-03-03 18:44:24 +0530

    Initial commit

---
+++ src/test/java/com/nikhil/microservices/springcloudconfigserver/SpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikhil.microservices.springcloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
